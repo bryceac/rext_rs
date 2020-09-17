2e50dabdd22855ad0ff0b665ce49dcfe63c9bbf4
Bryce Campbell <tonyhawk2100@gmail.com>
2020-09-17 11:18:14 -0700

    start work of implementing renaming

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{ App, Arg };
-use std::{ fs, path::PathBuf};
+use std::{ ffi::OsStr, fs, path::PathBuf};
 use walkdir::{DirEntry, WalkDir };
 
 #[cfg(target_os = "windows")]
@@ -16,6 +16,18 @@ fn main() {
     .arg(Arg::with_name("directory")
         .about("directory to use")
         .takes_value(true)
+    )
+    .arg(
+        Arg::with_name("old_extension")
+        .about("extension to replace")
+        .takes_value(true)
+        .required(true)
+    )
+    .arg(
+        Arg::with_name("new_extension")
+        .about("the extension to switch to")
+        .takes_value(true)
+        .required(true)
     )
     .arg(
         Arg::with_name("recursive")
@@ -57,6 +69,9 @@ fn main() {
         fs::canonicalize(PathBuf::from(".")).unwrap()
     };
 
+    let extension = matches.value_of("old_extension").unwrap();
+    let new_extension = matches.value_of("new_extension").unwrap();
+
     let recursive = if matches.is_present("recursive") {
         true
     } else {
@@ -75,18 +90,32 @@ fn main() {
         false
     };
 
-    rename(directory.to_str().unwrap_or(""), recursive, hidden, verbose)
+    rename(directory.to_str().unwrap_or(""), extension, new_extension, recursive, hidden, verbose)
 }
 
-fn rename(dir: &str, recursive: bool, include_hidden: bool, verbose: bool) {
+fn rename(dir: &str, old_extension: &str, new_extension: &str, recursive: bool, include_hidden: bool, verbose: bool) {
     let walker = if recursive {
         WalkDir::new(dir).into_iter()
     } else {
         WalkDir::new(dir).max_depth(1).into_iter()
     };
 
-    for item in walker.filter_entry(|e| is_hidden(e) == include_hidden) {
+    for item in walker.filter_entry(|e| {
+        if include_hidden {
+            is_hidden(e) || !is_hidden(e)
+        } else {
+            !is_hidden(e)
+        }
+    }) {
+        if let Ok(entry) = item {
+            if let (Some(file_extension), Some(file_name)) = (entry.path().extension().and_then(OsStr::to_str), entry.path().file_stem().and_then(OsStr::to_str)) {
+                if file_extension == old_extension {
+                    let new_file_name = format!("{}.{}", file_name, new_extension);
 
+                    let new_path = entry.path().with_file_name(new_file_name);
+                }
+            }
+        }
     }
 }
 
